3f3da7ffb3f6fc5bbb1f0cde2f0acbf0f5d62afd
akrama23 <arundelfootball23@gmail.com>
2023-08-14 23:58:19 -0400

    fetch from api complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import { Component } from 'react';
-
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component{
@@ -10,19 +8,30 @@ class App extends Component{
     this.state = {
       monsters:[],
     };
-  }
+  };
 
   componentDidMount(){
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((res) => res.json())
-      .then((users) => console.log(users))
+      .then((users) => this.setState(() => {
+        return { monsters: users}
+      },
+      console.log(this.state)
+      ));
 
-  }
+  };
 
   render(){
     return (
       <div className="App">
-        
+        {this.state.monsters.map((monster)=> {
+          return(
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          )
+        })}
+        hello
       </div>
     );
   }
