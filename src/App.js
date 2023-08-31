ea8e62718f289cd68b51f30dc8e62dac83de8ed9
akrama23 <arundelfootball23@gmail.com>
2023-08-30 22:17:50 -0400

    testing and fetch

--- src/App.js
+++ src/App.js
@@ -1,41 +1,47 @@
-import { Component } from 'react';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
 
-class App extends Component{
-  constructor(){
+class App extends Component {
+  constructor() {
     super();
 
     this.state = {
-      monsters:[],
+      monsters: [],
     };
-  };
 
-  componentDidMount(){
+    console.log("constructor 1");
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount 3");
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((users) => this.setState(() => {
-        return { monsters: users}
-      },
-      console.log(this.state)
-      ));
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
+  }
 
-  };
-// added monster id
-  render(){
+  render() {
+    console.log("render 2");
     return (
       <div className="App">
-        {this.state.monsters.map((monster)=> {
-          return(
+        {this.state.monsters.map((monster) => {
+          return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
             </div>
-          )
+          );
         })}
-        hello
       </div>
     );
   }
-
 }
 
 export default App;
